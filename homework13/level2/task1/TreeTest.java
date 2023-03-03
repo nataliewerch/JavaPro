a51fb62a7eb408bd571ebc5138acd429ebbfe77c
nataliewerch <115482667+nataliewerch@users.noreply.github.com>
2023-03-03 09:17:37 +0100

    Add files via upload

---
+++ homework13/level2/task1/TreeTest.java
@@ -0,0 +1,21 @@
+package homework13.level2.task1;
+
+public class TreeTest {
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        tree.add(13);
+        tree.add(17);
+        tree.add(15);
+        tree.add(12);
+        tree.add(25);
+        tree.add(23);
+        tree.add(21);
+        tree.add(14);
+        tree.print();
+
+    }
+
+
+
+
+}
